287c42886304330c3b82ad6d193e9854cacada2c
HITESH SAINI <hs@hiteshs.netai.net>
2017-01-18 07:47:38 +0530

    aaa

---
+++ public/js/realtime/old_data.js
@@ -0,0 +1,226 @@
+var data = [
+		{
+			activeUsers: 55,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 3,
+
+			device: [
+				{ name: "Desktop", users: 39 },
+				{ name: "Tablet", users: 5 },
+				{ name: "Mobile", users: 11 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 38 },
+				{ name: "Direct", users: 8 },
+				{ name: "Paid", users: 5 },
+				{ name: "Referral", users: 4 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 8 },
+				{ name: "Women Clothing", users: 9 },
+				{ name: "Gadgets", users: 10 },
+				{ name: "Books", users: 5 },
+				{ name: "Others", users: 23 }
+			],
+			states: [
+				{ name: "Others", users: 16 },
+				{ name: "Pennsylvania", users: 4 },
+				{ name: "Florida", users: 5 },
+				{ name: "Texas", users: 7 },
+				{ name: "New York", users: 11 },
+				{ name: "California", users: 12 },
+			]
+		},
+		{
+			activeUsers: 56,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 3,
+
+			device: [
+				{ name: "Desktop", users: 40 },
+				{ name: "Tablet", users: 5 },
+				{ name: "Mobile", users: 11 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 39 },
+				{ name: "Direct", users: 8 },
+				{ name: "Paid", users: 5 },
+				{ name: "Referral", users: 4 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 9 },
+				{ name: "Women Clothing", users: 9 },
+				{ name: "Gadgets", users: 10 },
+				{ name: "Books", users: 5 },
+				{ name: "Others", users: 23 }
+			],
+			states: [
+				{ name: "Others", users: 17 },
+				{ name: "Pennsylvania", users: 4 },
+				{ name: "Florida", users: 5 },
+				{ name: "Texas", users: 7 },
+				{ name: "New York", users: 11 },
+				{ name: "California", users: 12 },
+			]
+		},
+		{
+			activeUsers: 57,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 3,
+
+			device: [
+				{ name: "Desktop", users: 41 },
+				{ name: "Tablet", users: 5 },
+				{ name: "Mobile", users: 11 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 39 },
+				{ name: "Direct", users: 9 },
+				{ name: "Paid", users: 5 },
+				{ name: "Referral", users: 4 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 8 },
+				{ name: "Women Clothing", users: 9 },
+				{ name: "Gadgets", users: 11 },
+				{ name: "Books", users: 5 },
+				{ name: "Others", users: 24 }
+			],
+			states: [
+				{ name: "Others", users: 17 },
+				{ name: "Pennsylvania", users: 4 },
+				{ name: "Florida", users: 5 },
+				{ name: "Texas", users: 7 },
+				{ name: "New York", users: 12 },
+				{ name: "California", users: 12 },
+			]
+		},
+		{
+			activeUsers: 58,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 3,
+
+			device: [
+				{ name: "Desktop", users: 42 },
+				{ name: "Tablet", users: 5 },
+				{ name: "Mobile", users: 11 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 40 },
+				{ name: "Direct", users: 8 },
+				{ name: "Paid", users: 6 },
+				{ name: "Referral", users: 4 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 9 },
+				{ name: "Women Clothing", users: 10 },
+				{ name: "Gadgets", users: 11 },
+				{ name: "Books", users: 4 },
+				{ name: "Others", users: 24 }
+			],
+			states: [
+				{ name: "Others", users: 17 },
+				{ name: "Pennsylvania", users: 4 },
+				{ name: "Florida", users: 6 },
+				{ name: "Texas", users: 7 },
+				{ name: "New York", users: 12 },
+				{ name: "California", users: 12 },
+			]
+		},
+		{
+			activeUsers: 59,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 4,
+
+			device: [
+				{ name: "Desktop", users: 43 },
+				{ name: "Tablet", users: 4 },
+				{ name: "Mobile", users: 12 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 41 },
+				{ name: "Direct", users: 8 },
+				{ name: "Paid", users: 6 },
+				{ name: "Referral", users: 4 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 9 },
+				{ name: "Women Clothing", users: 10 },
+				{ name: "Gadgets", users: 11 },
+				{ name: "Books", users: 5 },
+				{ name: "Others", users: 24 }
+			],
+			states: [
+				{ name: "Others", users: 17 },
+				{ name: "Pennsylvania", users: 4 },
+				{ name: "Florida", users: 6 },
+				{ name: "Texas", users: 7 },
+				{ name: "New York", users: 12 },
+				{ name: "California", users: 13 },
+			]
+		},
+		{
+			activeUsers: 60,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 4,
+
+			device: [
+				{ name: "Desktop", users: 43 },
+				{ name: "Tablet", users: 5 },
+				{ name: "Mobile", users: 12 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 40 },
+				{ name: "Direct", users: 9 },
+				{ name: "Paid", users: 6 },
+				{ name: "Referral", users: 5 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 9 },
+				{ name: "Women Clothing", users: 10 },
+				{ name: "Gadgets", users: 12 },
+				{ name: "Books", users: 5 },
+				{ name: "Others", users: 24 }
+			],
+			states: [
+				{ name: "Others", users: 18 },
+				{ name: "Pennsylvania", users: 3 },
+				{ name: "Florida", users: 6 },
+				{ name: "Texas", users: 8 },
+				{ name: "New York", users: 12 },
+				{ name: "California", users: 13 },
+			]
+		},
+		{
+			activeUsers: 61,
+			pageViewsPerSecondLowerLimit: 0,
+			pageViewsPerSecondUpperLimit: 5,
+
+			device: [
+				{ name: "Desktop", users: 44 },
+				{ name: "Tablet", users: 5 },
+				{ name: "Mobile", users: 12 }
+			],
+			trafficMedium: [
+				{ name: "Organic", users: 42 },
+				{ name: "Direct", users: 9 },
+				{ name: "Paid", users: 5 },
+				{ name: "Referral", users: 5 }
+			],
+			categories: [
+				{ name: "Men Clothing", users: 9 },
+				{ name: "Women Clothing", users: 10 },
+				{ name: "Gadgets", users: 12 },
+				{ name: "Books", users: 5 },
+				{ name: "Others", users: 25 }
+			],
+			states: [
+				{ name: "Others", users: 18 },
+				{ name: "Pennsylvania", users: 3 },
+				{ name: "Florida", users: 6 },
+				{ name: "Texas", users: 8 },
+				{ name: "New York", users: 13 },
+				{ name: "California", users: 13 },
+			]
+		}
+	];
